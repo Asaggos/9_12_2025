68678744c48a1a5dc981f523fd0df45c53a5f7ac
SophieAdam95 <e1@akmith-edu.local>
2025-11-25 19:18:32 +0200

    Askhsh 1

---
+++ askhseis25.11.py
@@ -0,0 +1,13 @@
+# #Askhsh 1
+
+# def whatVowels(s):
+#     vowels = {'a', 'e', 'i', 'o', 'u', 'y'}
+#     s1 = []
+#     for i in s.lower():
+#         if i in vowels:
+#             s1.append(i)
+#     return s1
+
+# leksh = whatVowels(input("Dwse leksh: "))
+# print ("".join(leksh))
+
