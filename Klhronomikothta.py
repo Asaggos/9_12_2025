e0e34fd63135ddf1b2a64d87ec2d653c735f3e6a
Aggelos <e1@akmith-edu.local>
2025-12-02 20:11:50 +0200

    Telos

--- Klhronomikothta.py
+++ Klhronomikothta.py
@@ -2,9 +2,23 @@ class Person():
     def __init__(self,name,age):
         self.name = name 
         self.age = age
+    
+    def printStats(self):
+        print(self.name, self.age)
 
 class Teacher(Person):
     def __init__(self,name,age,salary,xronia):
         Person.__init__(name,age)
         self.salary = salary
         self.xronia = xronia
+
+class Student(Person):
+    def __init__(self, name, age, AM):
+        super().__init__(name, age)
+        self.AM = AM
+
+    def getAM(self):
+        print(self.AM)
+
+mathitis = Student("Nikos",25,144)
+mathitis.printStats(), mathitis.getAM()
