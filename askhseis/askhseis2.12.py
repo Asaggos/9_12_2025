4e08095f028016bed0445ca8ec92d1f6acff5e12
Aggelos <e1@akmith-edu.local>
2025-12-09 19:41:50 +0200

    Ask3 merge sto askhseis2.12

--- askhseis/askhseis2.12.py
+++ askhseis/askhseis2.12.py
@@ -26,3 +26,23 @@
     
 # Orthogonio = Rectangle(3,3)
 # print("Emvadon: " ,Orthogonio.CalcArea())
+
+#Askhsh 3
+
+p = 3.14159
+
+class Circle():
+    
+    def __init__(self,radius):
+        self.radius = radius
+
+    def Emvado(self):
+        return p * (self.radius**2)
+    
+    def perimetros(self):
+        return 2 * p * self.radius
+    
+c = Circle(5)
+
+print ("Emvado= ", c.Emvado())
+print ("Perimetros= " , c.perimetros())
