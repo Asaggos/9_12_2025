cfb89919c66b829e468138d1be8f82cc68934947
Aggelos <e1@akmith-edu.local>
2025-12-09 20:33:15 +0200

    Prosthetw ask4 stis ypoloipes

--- askhseis/askhseis2.12.py
+++ askhseis/askhseis2.12.py
@@ -29,20 +29,61 @@
 
 #Askhsh 3
 
-p = 3.14159
+# p = 3.14159
 
-class Circle():
+# class Circle():
     
-    def __init__(self,radius):
-        self.radius = radius
+#     def __init__(self,radius):
+#         self.radius = radius
 
-    def Emvado(self):
-        return p * (self.radius**2)
+#     def Emvado(self):
+#         return p * (self.radius**2)
     
-    def perimetros(self):
-        return 2 * p * self.radius
+#     def perimetros(self):
+#         return 2 * p * self.radius
     
-c = Circle(5)
+# c = Circle(5)
 
-print ("Emvado= ", c.Emvado())
-print ("Perimetros= " , c.perimetros())
+# print ("Emvado= ", c.Emvado())
+# print ("Perimetros= " , c.perimetros())
+
+
+
+# #Askhsh 4
+
+# import math
+
+# class Vector():
+#     def __init__(self,x,y):
+#         self.x = x
+#         self.y = y
+
+#     def ReadData(self):
+#         x= int(input("X: "))
+#         y= int(input("Y: "))
+
+#         return x, y
+
+#     def SetData(self,xi,yi):
+#         self.x=xi
+#         self.y=yi
+
+#     def PrintData(self):
+#         print("X = " , self.x ,"\nY = ", self.y)
+
+#     def metro(self):
+#         return   math.sqrt(self.x**2 + self.y**2)
+
+
+# v1 = Vector(1,2)
+# v1.PrintData()
+
+# v2 = Vector(0,0)
+# x , y = v2.ReadData()
+# v2.SetData(x,y)
+# v2.PrintData()
+
+# metro1 = v1.metro()
+# metro2 = v2.metro()
+
+# print(f"Metro1: {metro1:.2f} \nMetro2: {metro2:.2f}")
